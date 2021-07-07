bcb5af4215ce7a87d3b59a541bf13a28ce2b044b
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-07-07 23:42:43 +0530

    Updating index.js for commit | chores

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function Wrork(string) {
-    if (typeof string !== "string") throw new TypeError("Work wants a string!");
+    if (typeof string !== "string") throw new TypeError("Work Function wants a string!");
     return string.replace(/\s/g, "");
   };
