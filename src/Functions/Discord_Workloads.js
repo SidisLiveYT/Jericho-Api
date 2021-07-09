02c52c1f61edd7dfe9fcf29f826d4b44a6348523
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-07-09 22:04:41 +0530

    fix(/src): fix for eslint errors on Build and Minor Changes

--- src/Functions/Discord_Workloads.js
+++ src/Functions/Discord_Workloads.js
@@ -1,10 +1,10 @@
 const { WebhookClient } = require('discord.js');
 
 module.exports = {
-  WebhookFunction(client, Embed, Embed_Options) {
+  WebhookFunction(client, Embed, EmbedOptions) {
     try {
-      if (!Embed_Options.ID || !Embed_Options.Token) throw TypeError('Invalid Discord Webhook ID or Token!');
-      const Webhook = new WebhookClient(Embed_Options.ID, Embed_Options.Token);
+      if (!EmbedOptions.ID || !EmbedOptions.Token) throw TypeError('Invalid Discord Webhook ID or Token!');
+      const Webhook = new WebhookClient(EmbedOptions.ID, EmbedOptions.Token);
       Webhook.send({ embeds: [Embed] });
       return true;
     } catch (error) { return error; }
