ab3736b11608a7b7e54f34ce965917825be34c08
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-07-10 17:22:57 +0530

    Fixing '#' issue in Embed Gen

--- src/Functions/Discord_Workloads.js
+++ src/Functions/Discord_Workloads.js
@@ -9,4 +9,20 @@ module.exports = {
       return true;
     } catch (error) { return error; }
   },
+  setOptions(options) {
+    /**
+          * @property {function} SetOptions Make the Options filtered with acceptable values
+          * @param {Object} options Package Options for Embed Gen
+          * @returns {Object} options for this.options
+          */
+
+    const Option = {
+      type: EmbedTypes.includes(`${options.type.toLowerCase().trim()}`) ? options.type.toLowerCase().trim() : 'null',
+      Webhook: !!options.Webhook,
+      Compilation: typeof options.Compilation === 'boolean' ? options.Compilation : false,
+      WebhookID: options.Webhook ? options.Webhook.ID : null,
+      WebhookToken: options.Webhook ? options.Webhook.Token : null,
+    };
+    return Option;
+  }
 };
