4bd3a2d2218e60961919215481b6fa84a7b8a90a
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-07-12 16:07:49 +0530

    feat(crytocurrency.js): added Crypto-Currency Data API

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const { EmbedGen } = require('./distributions/embedgen');
 const { JokeGenerator } = require('./distributions/JokesGen');
 const { MemesGenerator } = require('./distributions/MemesGen');
 const { Shortner } = require('./distributions/ShortnerLinks');
+const { CryptoSector } = require('./distributions/CryptoCurrency');
 
 module.exports = {
   EmbedGenerator: EmbedGen,
   Jokes: JokeGenerator,
   Memes: MemesGenerator,
   ShortLink: Shortner,
+  Crypto: CryptoSector,
 };
